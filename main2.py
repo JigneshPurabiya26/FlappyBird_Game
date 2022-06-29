95511e4565af0cd077dce15c6cbab2b6a4053a49
JigneshPurabiya26 <jignesh26purabiya@gmail.com>
2022-06-29 17:08:32 +0530

    Further calculations added in the main file

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ pygame.init()# this particular function initializes all the modules of the pygam
 
 
 # Now giving the height and width to the screen
-width = 1100
+width = 1300
 height = 620
 
 # now creating the display using the function pygame.display.set_mode
@@ -29,20 +29,62 @@ pygame.display.set_caption('Flappy Bird by Jignesh')
 class Game:
     def __init__(self):
         self.gameOn = True
-        self.BirdX = 100
-        self.BirdY = 100
+        self.birdX = 100
+        self.birdY = 100
+        self.pipesX = [width,width+250,width+450,width+650,width+850,width+1050,width+1250]
+        self.lowerPipeY = [self.randomPipe(),self.randomPipe(),self.randomPipe(),self.randomPipe(),self.randomPipe(),self.randomPipe(),self.randomPipe(),]
+        self.upperPipeY = [self.randomRotatedPipe(),self.randomRotatedPipe(),self.randomRotatedPipe(),self.randomRotatedPipe(),self.randomRotatedPipe(),self.randomRotatedPipe(),self.randomRotatedPipe(),]
+        self.gravity=0
+        self.pipevel=0
+        self.score=0
+
+
+    def movingpipe(self):
+        for i in range(0,7):
+            self.pipesX [i] += -self.pipevel
+
+        for i in range(0,7):
+            if(self.pipesX[i] < -50):
+                self.pipesX[i] = width + 100
+                self.lowerPipeY[i] = self.randomPipe()
+                self.upperPipeY[i] = self.randomRotatedPipe()
+
+    def randomPipe(self):
+        return random.randrange(int(height/2)+70, height-220) 
+        #random.randrange function will give us random x,y coordinates 
+        #here we have given the coordinates for the pipes
+        #i.e for eg: if h=10 then h/2+2 is x i.e x=7 and h-2 will be y=8 so between these two numbers we can get the pipes
+
+    def randomRotatedPipe(self):
+        return random.randrange(-int(height/2)+70,-100)
 
     def mainGame(self):
         while self.gameOn:
+            #event handling is done in the below code
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()    
 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.pipevel = 0.7
 
             #blitting images
             screen.blit(background,(0,0))
-            screen.blit(Bird, (self.BirdX,self.BirdY))
+
+            for i in range(0,7):
+                #lowerpipe
+                screen.blit(pipe,(self.pipesX[i],self.lowerPipeY[i]))
+
+                #upperpipe
+                screen.blit(rotatedpipe,(self.pipesX[i],self.upperPipeY[i]))
+
+
+            screen.blit(Bird, (self.birdX,self.birdY))
+
+            #moving pipe
+            self.movingpipe()
 
             pygame.display.update()
 
